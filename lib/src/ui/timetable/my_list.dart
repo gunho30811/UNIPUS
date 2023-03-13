c2649e62def98878afe2c78af3825db878741ceb
pentel1 <chs5310@naver.com>
2023-03-14 00:28:33 +0900

    project : init

---
+++ lib/src/ui/timetable/my_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class My_List extends StatelessWidget {
+  const My_List({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
---
+++ lib/src/ui/timetable/timetable.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../authentication/login.dart';
+
+
+
+class Timetable extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
