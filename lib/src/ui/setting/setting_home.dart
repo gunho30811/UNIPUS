12d5ea8f1769b6c4f167643d277f619d870d6c29
BongGeunKR <bbg3813@gmail.com>
2023-03-15 00:07:22 +0900

    sizehelper 추가
    sizehelpers .dart 를 넣어봄

--- lib/src/ui/setting/setting_home.dart
+++ lib/src/ui/setting/setting_home.dart
@@ -1,3 +1,4 @@
+import 'package:chur/src/ui/sizes_helpers.dart';
 import 'package:flutter/material.dart';
 
 class SettingHome extends StatelessWidget {
@@ -24,114 +25,117 @@ class SettingHome extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        width: 300,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SizedBox(
-                              child: Text('이름'),
-                            ),
-                            SizedBox(
-                              child: Text('수정하기'),
-                            ),
-                          ],
+            Flexible(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: displayWidth(context)*0.9,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                child: Text('이름'),
+                              ),
+                              SizedBox(
+                                child: Text('수정하기'),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      SizedBox(
-                        width: 300,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(Icons.notifications_none),
-                                SizedBox(
-                                  width: 15,
-                                ),
-                                SizedBox(
-                                  child: Text('알림 설정'),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              child: Text('버튼자리'),
-                            ),
-                          ],
+                        SizedBox(
+                          height: 5,
                         ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      SizedBox(
-                        width: 300,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(Icons.palette_outlined),
-                                SizedBox(
-                                  width: 15,
-                                ),
-                                SizedBox(
-                                  child: Text('카테고리 설정'),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              child: Text('설정'),
-                            ),
-                          ],
+                        SizedBox(
+                          width: 300,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(Icons.notifications_none),
+                                  SizedBox(
+                                    width: 15,
+                                  ),
+                                  SizedBox(
+                                    child: Text('알림 설정'),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                child: Text('버튼자리'),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      SizedBox(
-                        width: 300,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Icon(Icons.access_alarm),
-                                SizedBox(
-                                  width: 15,
-                                ),
-                                SizedBox(
-                                  child: Text('주간 시간표 시작시간 설정'),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              width: 300,
-                              child: Text('마감 시간 12:00 PM'),
-                            ),
-                            SizedBox(
-                              width: 300,
-                              child: Text('시작 시간 12:00 AM'),
-                            ),
-                          ],
+                        SizedBox(
+                          height: 5,
                         ),
-                      ),
-                    ],
-                  ),
-                  Container(),
-                  Container(),
-                ],
+                        SizedBox(
+                          width: 300,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(Icons.palette_outlined),
+                                  SizedBox(
+                                    width: 15,
+                                  ),
+                                  SizedBox(
+                                    child: Text('카테고리 설정'),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                child: Text('설정'),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        SizedBox(
+                          width: 300,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Icon(Icons.access_alarm),
+                                  SizedBox(
+                                    width: 15,
+                                  ),
+                                  SizedBox(
+                                    child: Text('주간 시간표 시작시간 설정'),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                width: 300,
+                                child: Text('마감 시간 12:00 PM'),
+                              ),
+                              SizedBox(
+                                width: 300,
+                                child: Text('시작 시간 12:00 AM'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(),
+                    Container(),
+                  ],
+                ),
               ),
             )
           ],
