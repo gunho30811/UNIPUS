2a5f94bc2fd3009047faa056c85fcd63cbeab23d
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2023-03-19 16:35:38 +0900

    loginScreen UI 생성
    1.로그인, 회원가입, 비밀번호 복구 기능 제공 인증 UI
    
    2.test용 이미지로 확인

---
+++ lib/src/ui/authentication/loginScreen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'dashboard_screen.dart';
+
+const users = const {
+  'dribbble@gmail.com': '12345',
+  'hunter@gmail.com': 'hunter',
+};
+
+class Login extends StatelessWidget {
+  Login({Key? key}) : super(key: key);
+
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  Future<String?> _authUser(LoginData data) {
+    debugPrint('Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'User not exists';
+      }
+      if (users[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupUser(LoginData data) {
+    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  Future<String?> _recoverPassword(String name) {
+    debugPrint('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'User not exists';
+      }
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      title: 'ECORP',
+      logo: 'assets/images/test.jpg',
+      onLogin: _authUser,
+      onSignup: _signupUser,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => DashboardScreen(),
+        ));
+      },
+      onRecoverPassword: _recoverPassword,
+    );
+  }
+}
