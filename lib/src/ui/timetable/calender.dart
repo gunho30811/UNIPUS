363b0b1baf5616de8b2653e6c7ac867de263e6c2
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2023-03-26 19:37:43 +0900

    calender 구현

--- lib/src/ui/timetable/calender.dart
+++ lib/src/ui/timetable/calender.dart
@@ -1,12 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   const Calendar({Key? key}) : super(key: key);
 
   @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  late CalendarFormat _calendarFormat;
+  late DateTime _focusedDay;
+  DateTime? _selectedDay;
+  Map<DateTime, List<String>> _events = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarFormat = CalendarFormat.month;
+    _focusedDay = DateTime.now();
+    _selectedDay = DateTime.now();
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+      });
+    }
+  }
+
+  List<String> _getEventsForDay(DateTime day) {
+    return _events[day] ?? [];
+  }
+
+  void _addMemoToSelectedDay(String memo) {
+    if (_selectedDay != null) {
+      if (_events[_selectedDay!] == null) {
+        _events[_selectedDay!] = [];
+      }
+      setState(() {
+        _events[_selectedDay!]!.add(memo);
+      });
+    }
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text("Calendar"),
+        centerTitle: true,
+        actions: [
+          PopupMenuButton<int>(
+            itemBuilder: (context) => [
+              for (var i = 1; i <= 12; i++)
+                PopupMenuItem(
+                  value: i,
+                  child: Text("$i월"),
+                ),
+            ],
+            onSelected: (value) {
+              setState(() {
+                _focusedDay = DateTime(DateTime.now().year, value);
+              });
+            },
+          ),
+        ],
+      ),
+      body: Column(
+          children: [
+      TableCalendar(
+      firstDay: DateTime.utc(2010, 10, 16),
+      lastDay: DateTime.utc(2030, 3, 14),
+      focusedDay: _focusedDay,
+      calendarFormat: _calendarFormat,
+      onFormatChanged: (format) {
+        if (_calendarFormat != format) {
+          setState(() {
+            _calendarFormat = format;
+          });
+        }
+      },
+      onPageChanged: (focusedDay) {
+        _focusedDay = focusedDay;
+      },
+      selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+      onDaySelected: _onDaySelected,
+      eventLoader: _getEventsForDay,
+    ),
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: ListView(
+                children: _selectedDay != null
+                    ? _getEventsForDay(_selectedDay!).map((event) {
+                  return ListTile(
+                    title: Text(event),
+                  );
+                }).toList()
+                    : [],
+              ),
+            ),
+          ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          TextEditingController memoController = TextEditingController();
+          await showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Add Memo'),
+              content: TextField(
+                controller: memoController,
+                decoration: InputDecoration(
+                  labelText: 'Memo',
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    _addMemoToSelectedDay(memoController.text);
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Add'),
+                ),
+              ],
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
