026a3bd6cce0c71bc5629cac9fee3b02727a46ac
pentel1 <chs5310@naver.com>
2023-04-02 15:53:57 +0900

    리포지토리 작업

--- lib/src/model/mylist/mylist.dart
+++ lib/src/model/mylist/mylist.dart
@@ -1,9 +1,41 @@
-import 'dart:ui';
+
+import 'dart:convert';
 
 class MyList {
-  String title;
-  String subtitle;
-  int color;
+  String? usercode;
 
-  MyList(this.title, this.subtitle, this.color);
+  String? title;
+  String? subtitle;
+  int? color;
+  String? data;
+  String? grade;
+
+  MyList({required this.usercode, required this.title, required this.subtitle, required this.color, required this.data, required this.grade});
+  //String contents;
+
+  Map<String, dynamic> toMap() => {
+    'usercode': this.usercode,
+    'title': this.title,
+    'subtitle': this.subtitle,
+    'color': this.color,
+    'data': this.data,
+    'grade': this.grade
+  };
+
+  factory MyList.fromJson(Map<String, dynamic> parsedJson){
+    return MyList(
+        usercode : parsedJson['usercode'],
+        title : parsedJson['title'],
+        subtitle : parsedJson['subtitle'],
+        color : parsedJson['color'],
+        data : parsedJson['data'],
+        grade : parsedJson['grade']
+    );
+  }
+
+
+
+
+
+
 }
--- lib/src/provider/mylist_provider.dart
+++ lib/src/provider/mylist_provider.dart
@@ -1,16 +1,19 @@
+import 'package:chur/src/repository/mylist_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../model/mylist/mylist.dart';
 
 class MyListProvider with ChangeNotifier {
-  List<MyList> _items = [
-    MyList('영단어 암기', 'p145 ~ p165', 0xffFFC93C),
-    MyList('수학 공부', 'p145 ~ p165', 0xffFFC93C),
-    MyList('영단어 암기', 'p145 ~ p165', 0xffFFC93C),
-  ];
+  late MyListRepository myListRepository;
+  // List<MyList> _items = [
+  //   MyList('usercode','영단어 암기', 'p145 ~ p165', 0xffFFC93C, "", ""),
+  //   MyList('usercode','수학 공부', 'p145 ~ p165', 0xffFFC93C, "", ""),
+  //   MyList('usercode','영단어 암기', 'p145 ~ p165', 0xffFFC93C, "", ""),
+  // ];
 
-  List<MyList> get mylist => _items;
+
+  List<MyList> get mylist => myListRepository.getMyList();
 
 
 
